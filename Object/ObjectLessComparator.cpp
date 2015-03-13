3c01737343e576c725a54457ffe591d438bf2760
RaulButuc <raulbutuc@gmail.com>
2015-03-13 20:11:30 +0000

    Removed unnecessary constructor/destructor

--- Object/ObjectLessComparator.cpp
+++ Object/ObjectLessComparator.cpp
@@ -2,12 +2,8 @@
 
 namespace Knapsack {
 
-	ObjectLessComparator::ObjectLessComparator() {}
-
 	bool ObjectLessComparator::operator()(const Object* pLeft, const Object* pRight) const {
 		return (*pLeft) < (*pRight);
 	}
-
-	ObjectLessComparator::~ObjectLessComparator() {}
 
 }
--- Object/ObjectLessComparator.h
+++ Object/ObjectLessComparator.h
@@ -7,9 +7,7 @@ namespace Knapsack {
 	class ObjectLessComparator {
 
 	public:
-		ObjectLessComparator();
 		bool operator()(const Object*, const Object*) const;
-		~ObjectLessComparator();
 
 	};
 
